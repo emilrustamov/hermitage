d9a3c08e1775b9bc6a305e4f1b9d8e50cc2c4041
emilrustamov <e.rustamov@lotta-tm.com>
2024-07-25 16:50:08 +0500

    jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap-new';
 import './bootstrap';
-import $ from 'jquery';
-window.$ = window.jQuery = $;
+// import $ from 'jquery';
+// window.$ = window.jQuery = $;
+import $ from 'jquery-slim';
+
+$(document).ready(function() {
+    console.log('Slim jQuery is ready');
+});
 import '@popperjs/core';
 
 import 'slick-carousel/slick/slick.min';
